ab0aaef6963bc2e8cb5a05fa97e4f69d65ba2ab1
Brendan Rogers <brendanr11@gmail.com>
2018-03-15 10:26:41 -0400

    Updated the JS. Added Timer. Added Cursor

--- js/app.js
+++ js/app.js
@@ -1,39 +1,86 @@
 // Self Executing Anonymous Function
-(function(){ 
+(function(){  console.log("JS Initialized");
 
-console.log("JS Initialized");
 
+// VARIABLES
+
+// BACKGROUND vars
 let field = document.getElementById("gameCanvas");
-let box1 = field.getContext("2d");
-let box2 = field.getContext("2d");
-let box3 = field.getContext("2d");
-let box4 = field.getContext("2d");
-let box5 = field.getContext("2d");
-let box6 = field.getContext("2d");
+let background = field.getContext("2d");
+let imgBackground = new Image();
+imgBackground.src = "images/battlefield.svg";
+imgBackground.classList.add("backgroundZ");
+// ALIEN vars
+let alien1 = field.getContext("2d");
+let imgAlien1 = new Image();
+imgAlien1.src = "images/alien1.svg"
+imgAlien1.classList.add("alien1Z");
 
-box1.rect(100,500,50,50);
-box1.fillStyle = '#AA0000';
-box1.fill();
+// SCORE vars
+let score = 0;
 
-box2.rect(200,100,50,50);
-box2.fillStyle = '#AA0000';
-box2.fill();
 
-box3.rect(400,600,50,50);
-box3.fillStyle = '#AA0000';
-box3.fill();
 
-box4.rect(700,350,50,50);
-box4.fillStyle = '#AA0000';
-box4.fill();
+// FUNCTIONS
 
-box5.rect(500,400,50,50);
-box5.fillStyle = '#AA0000';
-box5.fill();
+// LOAD BACKGROUND CONTEXT
+imgBackground.onload = function() {
+	// set canvas size
+	field.width = 800;
+	field.height = 800;
+	// draw background(DOM element) as JS IMG at (xLocation, yLocation, xLength, yLength)
+    background.drawImage(imgBackground, 0, 0, 800, 800);
+    // confirm load
+    console.log("Background Loaded"); }
 
-box6.rect(250,200,50,50);
-box6.fillStyle = '#AA0000';
-box6.fill();
+// LOAD ALIEN1 CONTEXT
+imgAlien1.onload = function() {
+	// apply imgAlien1 to DOM Variable alien1
+	alien1.drawImage(imgAlien1, 75, 500, 100, 100);
+	// confirm load
+	console.log("alien1 is loaded");
+}
+
+// EVENT HANDLERS
+
+
+// TEST TIMER
+function startTimer(duration, display) {
+    let timer = duration, minutes, seconds;
+
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    let startMinutes = 60 * 2,
+        display = document.querySelector('#time');
+    startTimer(startMinutes, display);
+};
+
 
 
 }) ();
+
+
+
+
+
+
+
+
+
+
+
+
